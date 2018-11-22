f911cd624eede0c220e91e8183d3132f20d477dc
Vadim Zendejas <vazendej@microsoft.com>
2018-11-22 16:30:46 +0100

    Added Express.js as Routing framework

--- index.js
+++ index.js
@@ -5,95 +5,103 @@
  * Date: Oct, 2018 
 */
 
-var http = require('http');
-var url = require('url');
+var express = require('express')
+var app = express();
+var utilities = require('./utilities.js');
 var mongoClient = require("mongodb").MongoClient;
 const uuidv4 = require('uuid/v4');
 
-var server = http.createServer(function(request, response) {
 
-    var url_parts = url.parse(request.url, true);
-    var query = url_parts.query;
-    var apikey = query.apikey;
 
-    if (apikey === undefined || process.env.APPSETTING_apikey === undefined || apikey != process.env.APPSETTING_apikey) {
-        response.writeHead(400, {"Content-Type": "text/plain"});
-        response.end("Unauthorized");
+app.get('/', function (req, res) {
+    if(!utilities.isValidSession(req)) {
+        res.status(400).send('Unauthorized');
+        return;
+    }
+    res.send("Hello world!")
+});
+
+app.get('/save', function (req, res) {
+    if(!utilities.isValidSession(req)) {
+        res.status(400).send('Unauthorized');
         return;
     }
 
-    var data = '';
-    request.on('data', function(chunk) {
-        data += chunk.toString();
-    });
-    request.on('end', function() {
+    console.log(req);
+
+    /* mongoClient.connect(process.env.APPSETTING_connectionStringPrimary, function (err, client) {
+
+        if (err) {
+            console.log(err);
+            throw err;
+        };
+
+        var dbo = client.db("onenotesummary");
+        var query = {};
+
+        if (utilities.tryParseJSON(data) !== false) data = JSON.parse(data);
+        if (data == "") data = {};
         
-        //console.log(data);
 
-        mongoClient.connect(process.env.APPSETTING_connectionStringPrimary, function (err, client) {
-
+        dbo.collection("pages").insertOne({id: uuidv4(), body: JSON.stringify(data)}, function(err, record){
             if (err) {
                 console.log(err);
-                throw err;
+                throw err
             };
-    
-            var dbo = client.db("onenotesummary");
-            var query = {};
+            console.log("Inserted record with id: " + record.insertedId);
 
-            if (tryParseJSON(data) !== false) data = JSON.parse(data);
-            if (data == "") data = {};
-            
-
-            dbo.collection("pages").insertOne({id: uuidv4(), body: JSON.stringify(data)}, function(err, record){
-                if (err) {
-                    console.log(err);
-                    throw err
-                };
-                console.log("Inserted record with id: " + record.insertedId);
-
-                response.writeHead(200, {"Content-Type": "text/plain"});
-                response.end("");
-            });
-
-            /*dbo.collection("pages").find(query).toArray(function(err, result) {
-                if (err) {
-                    console.log(err);
-                    throw err
-                };
-                console.log(result);
-                client.close();
-    
-                response.writeHead(200, {"Content-Type": "text/plain"});
-                response.end(JSON.stringify(data));
-            });*/
+            response.writeHead(200, {"Content-Type": "text/plain"});
+            response.end("");
         });
 
+        dbo.collection("pages").find(query).toArray(function(err, result) {
+            if (err) {
+                console.log(err);
+                throw err
+            };
+            console.log(result);
+            client.close();
+
+            response.writeHead(200, {"Content-Type": "text/plain"});
+            response.end(JSON.stringify(data));
+        });
+    }); */
+
+    res.send(req.body);
+});
+
+app.get('/list', function (req, res) {
+    if(!utilities.isValidSession(req)) {
+        res.status(400).send('Unauthorized');
+        return;
+    }
+
+    mongoClient.connect(process.env.APPSETTING_connectionStringPrimary, function (err, client) {
+
+        if (err) {
+            console.log(err);
+            throw err;
+        };
+
+        var dbo = client.db("onenotesummary");
+        var query = {};
+
+        dbo.collection("pages").find(query).toArray(function(err, result) {
+            if (err) {
+                console.log(err);
+                throw err
+            };
+            console.log(result);
+            client.close();
+
+            res.send(data)
+
+        });
     });
-
-
     
-
 });
 
 
-function tryParseJSON (jsonString){
-    try {
-        var o = JSON.parse(jsonString);
+var port = process.env.PORT || 1338;
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
 
-        // Handle non-exception-throwing cases:
-        // Neither JSON.parse(false) or JSON.parse(1234) throw errors, hence the type-checking,
-        // but... JSON.parse(null) returns null, and typeof null === "object", 
-        // so we must check for that, too. Thankfully, null is falsey, so this suffices:
-        if (o && typeof o === "object") {
-            return o;
-        }
-    }
-    catch (e) { }
-
-    return false;
-};
-
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
---
+++ utilities.js
@@ -0,0 +1,32 @@
+//var url = require('url');
+
+exports.isValidSession = function(request) {
+    //var url_parts = url.parse(request.url, true);
+    //var query = url_parts.query;
+    //var apikey = query.apikey;
+    var apikey = request.query.apikey;
+    var originalApiKey = process.env.APPSETTING_apikey || '42c080a3-a136-42da-93e5-91d1d2b47eec';
+
+    if (apikey === undefined || apikey != originalApiKey) {
+        return false;
+    }
+
+    return true;
+};
+
+exports.tryParseJSON = function(jsonString) {
+    try {
+        var o = JSON.parse(jsonString);
+
+        // Handle non-exception-throwing cases:
+        // Neither JSON.parse(false) or JSON.parse(1234) throw errors, hence the type-checking,
+        // but... JSON.parse(null) returns null, and typeof null === "object", 
+        // so we must check for that, too. Thankfully, null is falsey, so this suffices:
+        if (o && typeof o === "object") {
+            return o;
+        }
+    }
+    catch (e) { }
+
+    return false;
+};

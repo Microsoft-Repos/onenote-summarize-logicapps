c95e654a650a998a4cf72e27062129c1a8cdfeb5
Vadim Zendejas <vazendej@microsoft.com>
2018-11-22 14:47:30 +0100

    Added object when empty

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ var server = http.createServer(function(request, response) {
         
         //console.log(data);
 
+        if (data == "") {
+            data = {id: Date.toString()};
+        }
+
         mongoClient.connect(process.env.APPSETTING_connectionStringPrimary, function (err, client) {
 
             if (err) {
